5109862e958be478a5ce7765602b807da33f4ad5
Ramiro Batallas <rbatallas@oshyn.com>
2024-03-02 03:25:29 -0500

    merge with main

---
+++ src/Feature/IACarousel/website/Controllers/SmartCarouselController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using WbbHackathon.Feature.IACarousel.Models;
+using WbbHackathon.Feature.IACarousel.Repositories;
+
+namespace WbbHackathon.Feature.IACarousel.Controllers
+{
+    public class SmartCarouselController : Controller
+    {
+        
+
+        public ActionResult GetSmartCarousel()
+        {
+            SmartCarouselModel model = new SmartCarouselModel();
+            return View("~/Views/SmartCarousel/SmartCarouselView.cshtml", model);
+        }
+    }
+}
