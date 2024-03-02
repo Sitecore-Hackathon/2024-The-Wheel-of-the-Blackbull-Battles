a88210949fe303445b732842745e63001ef3161d
Freddy Rueda <frueda@oshyn.com>
2024-03-02 04:24:25 -0500

    Create method to add images to media library

---
+++ src/Feature/IACarousel/website/Repositories/IMediaItemRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace WbbHackathon.Feature.IACarousel.Repositories
+{
+    public interface IMediaItemRepository
+    {
+        List<string> CreateMediaItem(string[] path, string id, string destination, string database);
+    }
+}
