eef69c19283138050f868e35fca43edce6058a65
Ramiro Batallas <rbatallas@oshyn.com>
2024-03-02 00:56:49 -0500

    Implement read Leonardo AI service

---
+++ src/Feature/IACarousel/website/Constants.cs
@@ -0,0 +1,14 @@
+﻿using Sitecore.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WbbHackathon.Feature.IACarousel
+{
+    public class Constants
+    {
+        public static string GenerationEndPoint = Settings.GetSetting("GenerationEndpoint");
+        public static string ApiToken = Settings.GetSetting("GenerationApiToken");
+    }
+}
---
+++ src/Feature/IACarousel/website/Models/Parameters/ImageGenerationParameters.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WbbHackathon.Feature.IACarousel.Models.Parameters
+{
+    public class ImageGenerationParameters
+    {
+        [JsonProperty("height")]
+        public int Height { get; set; }
+
+        [JsonProperty("width")]
+        public int Width { get; set; }
+
+        [JsonProperty("modelId")]
+        public string ModelId { get; set; }
+
+        [JsonProperty("prompt")]
+        public string Prompt { get; set; }
+    }
+}
