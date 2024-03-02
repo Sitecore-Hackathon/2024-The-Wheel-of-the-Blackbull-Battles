e265e04ecaac2d30ea99945c564eefa1d1d3fdae
Ivan Toromoreno <ivan.toromorenos@gmail.com>
2024-03-02 15:29:18 -0500

    Update jquery implementation

--- src/Feature/IACarousel/website/Controllers/SmartCarouselController.cs
+++ src/Feature/IACarousel/website/Controllers/SmartCarouselController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
+using WbbHackathon.Feature.IACarousel.Models;
 using WbbHackathon.Feature.IACarousel.Repositories;
 
 namespace WbbHackathon.Feature.IACarousel.Controllers
@@ -22,7 +23,8 @@ namespace WbbHackathon.Feature.IACarousel.Controllers
 
         public ActionResult GetSmartCarousel()
         {           
-            var model = _imageGenerationRepository.GenerateImages();            
+            // var model = _imageGenerationRepository.GenerateImages();   
+            SmartCarouselModel model = GenerateMockCarouselData();
             return View(SmartCarouselView, model);
         }
 
@@ -30,6 +32,21 @@ namespace WbbHackathon.Feature.IACarousel.Controllers
         {
             var model = _userSmartCarouselRepository.GetCarouselImages();
             return View(UserSmartCarouselView, model);
+        }
+
+        private SmartCarouselModel GenerateMockCarouselData ()
+        {
+            SmartCarouselModel model = new SmartCarouselModel();
+            SmartCarouselImage smartCarouselImage1 = new SmartCarouselImage() { ID = "d41275f3-6d6d-48ec-95e4-7b50b0a80589", Url = "https://cdn.leonardo.ai/users/911ec922-1f78-464b-b890-c38ced86ca94/generations/10dd0c83-edb0-45c4-931b-b17ee6e8f89b/Default_Realistic_photo_animals_jungle_africa_0.jpg" };
+            SmartCarouselImage smartCarouselImage2 = new SmartCarouselImage() { ID = "4087033f-5f04-436e-ac25-7a0a34fcc250", Url = "https://cdn.leonardo.ai/users/911ec922-1f78-464b-b890-c38ced86ca94/generations/10dd0c83-edb0-45c4-931b-b17ee6e8f89b/Default_Realistic_photo_animals_jungle_africa_1.jpg" };
+            SmartCarouselImage smartCarouselImage3 = new SmartCarouselImage() { ID = "6bbcd0ec-5713-4971-8bfd-ce267df80248", Url = "https://cdn.leonardo.ai/users/911ec922-1f78-464b-b890-c38ced86ca94/generations/10dd0c83-edb0-45c4-931b-b17ee6e8f89b/Default_Realistic_photo_animals_jungle_africa_2.jpg" };
+            SmartCarouselImage smartCarouselImage4 = new SmartCarouselImage() { ID = "a32fa490-a557-4edf-8f38-48015edfcd0c", Url = "https://cdn.leonardo.ai/users/911ec922-1f78-464b-b890-c38ced86ca94/generations/10dd0c83-edb0-45c4-931b-b17ee6e8f89b/Default_Realistic_photo_animals_jungle_africa_3.jpg" };
+            model.Images.Add(smartCarouselImage1);
+            model.Images.Add(smartCarouselImage2);
+            model.Images.Add(smartCarouselImage3);
+            model.Images.Add(smartCarouselImage4);
+            model.Prompt = "Test prompt";
+            return model;
         }
 
         [HttpPost]
